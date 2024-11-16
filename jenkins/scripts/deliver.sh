030fa74276c233f9c763cbb1598731a11a47fed2
Vijayalakshmi-KL <97176302+Vijayalakshmi-KL@users.noreply.github.com>
2024-11-16 15:03:42 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,4 +23,12 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+##java -jar target/${NAME}-${VERSION}.jar
+java -jar "$(find target -name '*.jar')"
+ls -l target/
+NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name | tr -d '\r')
+VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version | tr -d '\r')
+
+java -jar "target/${NAME}-${VERSION}.jar"
+
+
